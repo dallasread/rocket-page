06f05fed1c48809ff1995ad102178b0ad6fde004
Dallas Read <dallas@excitecreative.ca>
2017-06-22 17:16:28 -0300

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var Rocket = {
-    Page: require('./page/js'),
-    Sections: {
-        Callout:   require('./sections/callout/js'),
-        Copyright: require('./sections/copyright/js'),
-        Hero:      require('./sections/hero/js'),
-        Browser:   require('./sections/browser/js'),
-        Speller:   require('./sections/speller/js'),
-        Timeline:  require('./sections/timeline/js')
-    }
-};
+// var Rocket = {
+//     Page: require('./page/js'),
+//     Sections: {
+//         Callout:   require('./sections/callout/js'),
+//         Copyright: require('./sections/copyright/js'),
+//         Hero:      require('./sections/hero/js'),
+//         Browser:   require('./sections/browser/js'),
+//         Speller:   require('./sections/speller/js'),
+//         Timeline:  require('./sections/timeline/js')
+//     }
+// };
 
-window.Rocket = Rocket;
+// window.Rocket = Rocket;
 
-module.exports = Rocket;
+// module.exports = Rocket;
